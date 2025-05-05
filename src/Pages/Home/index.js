473e3324975622f4de6d05e6f40552ea3592b078
Galya777 <galdodova@abv.bg>
2025-05-05 16:23:50 +0300

    added a slider

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -1,6 +1,29 @@
+import Slider from "react-slick";
+
+
 const Home =()=>{
+  var settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1
+  };
     return(
         <>
+           <div className="main-section">
+
+           <div className="slider-wrapper">
+  <Slider {...settings}>
+    <img src="/images/slider1.jpeg" alt="slide 1" />
+    <img src="/images/slider2.jpg" alt="slide 2" />
+    <img src="/images/slider3.jpg" alt="slide 3" />
+    <img src="/images/slider4.jpg" alt="slide 4" />
+    <img src="/images/slider5.jpg" alt="slide 5" />
+  </Slider>
+</div>
+
+
 <div className="vertical-menu-categories">
   <button><i className="fas fa-utensils"></i> Appetizers</button>
   <button><i className="fas fa-egg"></i> Breakfast</button>
@@ -14,6 +37,8 @@ const Home =()=>{
 </div>
 
 
+
+</div>
 
         </>
     )
